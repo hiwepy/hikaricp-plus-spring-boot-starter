29136b978498bd8aa17a1202e1bba77eb4a0cfca
wandl <hnxyhcwdl1003@163.com>
2019-12-27 17:49:13 +0800

    修改

--- src/main/java/com/zaxxer/hikari/spring/boot/HikaricpShardingJdbcAutoConfiguration.java
+++ src/main/java/com/zaxxer/hikari/spring/boot/HikaricpShardingJdbcAutoConfiguration.java
@@ -1,27 +1,11 @@
 package com.zaxxer.hikari.spring.boot;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.sql.DataSource;
-
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.util.CollectionUtils;
-
-import com.zaxxer.hikari.HikariDataSource;
-import com.zaxxer.hikari.spring.boot.ds.DynamicRoutingDataSource;
-import com.zaxxer.hikari.spring.boot.ds.HikaricpDataSourceProperties;
-import com.zaxxer.hikari.spring.boot.util.HikariDataSourceUtils;
-
-import io.shardingsphere.shardingjdbc.api.MasterSlaveDataSourceFactory;
-import io.shardingsphere.shardingjdbc.api.ShardingDataSourceFactory;
 
 /**
  * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
@@ -43,7 +27,7 @@ public class HikaricpShardingJdbcAutoConfiguration {
 	 * @param basicProperties {@link DataSourceProperties} 参数对象
 	 * @param hikariProperties {@link HikaricpProperties} 参数对象
 	 * @return {@link HikariDataSource} 数据源
-	 */
+	
 	@Bean
 	@Primary
 	public DataSource dataSource(DataSourceProperties basicProperties, HikaricpProperties hikariProperties) {
@@ -100,6 +84,6 @@ public class HikaricpShardingJdbcAutoConfiguration {
         	hikariProperties.setDriverClassName(basicProperties.determineDriverClassName());
         }
         return hikariProperties;
-	}
+	} */
 
 }
