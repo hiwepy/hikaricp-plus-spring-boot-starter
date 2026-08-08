61458214b494b4fa11c644fc444ad5020242772e
wandl-6A72h <hiwepy@gmail.com>
2026-08-09 00:00:15 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.2.x branch

--- src/main/java/com/zaxxer/hikari/spring/boot/HikaricpWithOnPrometheusAutoConfiguration.java
+++ src/main/java/com/zaxxer/hikari/spring/boot/HikaricpWithOnPrometheusAutoConfiguration.java
@@ -16,7 +16,7 @@ import io.prometheus.client.CollectorRegistry;
 
 /**
  * 基于Prometheus监控平台的HikariDataSource监控
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 @Configuration
 @ConditionalOnBean( HikariDataSource.class )
