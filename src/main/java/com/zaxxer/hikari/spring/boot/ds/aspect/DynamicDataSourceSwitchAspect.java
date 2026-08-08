42dd2159a44bc626ceb264b7014e599e9fc7561e
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:59:50 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 2.3.x branch

--- src/main/java/com/zaxxer/hikari/spring/boot/ds/aspect/DynamicDataSourceSwitchAspect.java
+++ src/main/java/com/zaxxer/hikari/spring/boot/ds/aspect/DynamicDataSourceSwitchAspect.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2017, hiwepy (https://github.com/easy-4-java).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -27,7 +27,7 @@ import com.zaxxer.hikari.spring.boot.ds.annotation.SwitchRepository;
 
 /**
  * 数据源自动切换切面
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 @Aspect
 @Component
